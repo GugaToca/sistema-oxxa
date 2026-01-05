d6475ea69f15a741119187ceb42e27168ab17c79
GugaToca <gustavotocalino08@outlook.com>
2026-01-05 10:12:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ renderMarkets(MARKET_DATA);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
+    navigator.serviceWorker.register("service-worker.js");
   });
 }
 
